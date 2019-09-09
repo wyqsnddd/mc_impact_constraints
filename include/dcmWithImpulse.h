a8d272f77fc1eb44cc24acb68126e94f79782f74
yuquan <wyqsnddd@gmail.com>
2019-09-10 00:28:31 +0200

    DCM constraint
    It works with hand-made support areas.
    
    Bug: The dcm area (static equilibrium area) generated by stephane leads to 'QP failed to run' despite that the constraint differene is negative

---
+++ include/dcmWithImpulse.h
@@ -0,0 +1,103 @@
+# pragma once
+
+#include <mc_prediction/mi_qpEstimator.h>
+#include <mc_solver/InequalityConstraint.h>
+#include <RBDyn/CoM.h>
+
+#include <math.h> 
+
+# include "constraintUtils.h"
+
+namespace mc_impact{
+
+template <typename supportContact, typename Point>
+struct dcmWithImpulse : public mc_solver::InequalityConstraint
+{
+
+dcmWithImpulse(mi_qpEstimator & predictor,
+                 const std::vector<supportContact> & supports,
+                 double dt,
+                 double impact_dt,
+                 const std::vector<Point> & vertexSet,
+		 bool debug = false);
+
+
+inline int maxInEq() const override
+  {
+    return static_cast<int>(iniVertexSet_.size());
+  }
+
+  inline std::string nameInEq() const override
+  {
+    return "dcmWithImpulse";
+  }
+
+
+  inline const Eigen::MatrixXd & A() const override
+  {
+    return A_;
+  }
+  inline const Eigen::VectorXd & bInEq() const override
+  {
+    return b_;
+  }
+
+  void computeAb() override;
+
+
+  inline const Eigen::MatrixXd & getA()
+  {
+    return A_;
+  }
+  inline const Eigen::VectorXd & getb()
+  {
+    return b_;
+  }
+
+// Debugging: 
+  Point centeroid_;
+  Eigen::VectorXd slopeVec_ = Eigen::Vector3d::Zero();
+
+  bool zmpTest_ = false;
+  Eigen::MatrixXd G_dcm_;
+  Eigen::VectorXd h_dcm_;
+
+  Eigen::Vector2d dcm_;
+  Eigen::Vector2d predicted_dcm_;
+  Eigen::VectorXd difference_; 
+
+  Eigen::MatrixXd A_dcm_;
+  bool pointInsideSupportPolygon(const Point & input);
+
+  inline void calcOmega( const double & c_z ){
+    assert(c_z >0.0);
+    omega_ =  sqrt(9.8/c_z);
+  }
+  inline const double & getOmega(){
+    return omega_; 
+  }
+private:
+  // Predictor
+  mi_qpEstimator & predictor_;
+   // Timestep
+  double dt_;
+  // Impact duration
+  double impact_dt_;
+  // Alpha vector
+  Eigen::VectorXd alpha_;
+
+  const std::vector<Point> iniVertexSet_;
+  double omega_ = 1.0;
+
+  bool debug_;
+
+  std::vector<supportContact> supports_;
+  Eigen::MatrixXd A_;
+  Eigen::VectorXd b_;
+
+
+  std::shared_ptr<rbd::CoMJacobian> comJacobianPtr_;
+  //Eigen::MatrixXd comJacobian_;
+};// end of dcmWithImpuplse
+
+} // end of namespace 
---
+++ src/dcmWithImpulse.cpp
@@ -0,0 +1,102 @@
+#include "dcmWithImpulse.h"
+
+namespace mc_impact {
+
+
+template<typename supportContact, typename Point>
+dcmWithImpulse<supportContact, Point>::dcmWithImpulse(mi_qpEstimator & predictor, 
+                 const std::vector<supportContact> & supports,
+                 double dt,
+                 double impact_dt,
+                 const std::vector<Point> & vertexSet,
+		 bool debug
+                 ):InequalityConstraint(predictor.getSimRobot().robotIndex()), predictor_(predictor), dt_(dt), impact_dt_(impact_dt), supports_(supports), iniVertexSet_(vertexSet), debug_(debug) {
+
+  int numVertex = static_cast<int>(iniVertexSet_.size());
+  A_dcm_ = Eigen::MatrixXd::Zero(numVertex, 6);
+
+  pointsToInequalityMatrix<Point>(iniVertexSet_, G_dcm_, h_dcm_, centeroid_, slopeVec_, 0.01, 100);
+ 
+  A_dcm_.block(0, 0, numVertex, 1) = G_dcm_.block(0, 1, numVertex, 1);
+  /// A(:,1) = -G_x
+  A_dcm_.block(0, 1, numVertex, 1) = - G_dcm_.block(0, 0, numVertex, 1);
+  /// A(:,5) = h 
+  A_dcm_.block(0, 5, numVertex, 1) = -h_dcm_; 
+
+
+  int nDof = predictor_.getSimRobot().mb().nrDof();
+  alpha_.resize(nDof);
+  A_.resize(numVertex, nDof);
+  b_.resize(numVertex);
+
+  difference_.resize(numVertex);
+  difference_.setZero();
+
+  comJacobianPtr_ = std::make_shared<rbd::CoMJacobian>(predictor_.getSimRobot().mb());
+
+  calcOmega( predictor_.getSimRobot().com().z() );
+}
+
+
+
+
+template <typename supportContact, typename Point>
+bool dcmWithImpulse<supportContact, Point>::pointInsideSupportPolygon(const Point & input){
+
+  Point result = G_dcm_*input - h_dcm_;
+
+  if (result[0] > 0)
+	  return false;
+  if (result[1] > 0)
+	  return false;
+  return true;
+}
+
+template <typename supportContact, typename Point>
+void dcmWithImpulse<supportContact, Point>::computeAb()
+{
+  const auto & robot = predictor_.getSimRobot();
+  int dof = predictor_.getSimRobot().mb().nrDof();
+  Eigen::MatrixXd comJacobian = comJacobianPtr_->jacobian(robot.mb(), robot.mbc());
+  //std::cout<<"comJacobian size is: "<<comJacobian.rows() << ", "<<comJacobian.cols()<<std::endl;
+  Eigen::MatrixXd jacDcm =   (comJacobian * predictor_.getJacobianDeltaAlpha()).block(0, 0, 2, dof);
+
+  A_ = dt_  * G_dcm_ * jacDcm; 
+  
+
+  rbd::paramToVector(robot.mbc().alpha, alpha_);
+
+  Eigen::Vector3d Com = robot.com();
+  Eigen::Vector3d ComVel = robot.comVelocity();
+
+  dcm_ =  Com.segment(0, 2) + ComVel.segment(0,2)/getOmega();
+
+  predicted_dcm_ = dcm_ + comJacobian.block(0, 0, 2, dof)*predictor_.getJointVelJump() /getOmega(); 
+
+  b_ = getOmega()*h_dcm_ 
+	  - G_dcm_*( dcm_*getOmega() + jacDcm*alpha_);
+
+  if(debug_)
+  {
+    Eigen::VectorXd temp = (rbd::dofToVector(predictor_.getSimRobot().mb(), predictor_.getSimRobot().mbc().alpha)
+                          + rbd::dofToVector(predictor_.getSimRobot().mb(), predictor_.getSimRobot().mbc().alphaD)
+                                * predictor_.getImpactModels().begin()->second->getTimeStep());
+
+    difference_ = A_ * rbd::dofToVector(predictor_.getSimRobot().mb(), predictor_.getSimRobot().mbc().alphaD) - b_;
+    std::cout<<"The dcm constraint difference is: "<<difference_<<std::endl;
+    std::cout<<"The dcm dq difference is: "<< G_dcm_*(dcm_ + 
+		    comJacobian.block(0, 0, 2, dof)*predictor_.getJointVelJump() /getOmega()) - h_dcm_<<std::endl;
+
+    std::cout<<"The dcm acc difference is: "<<G_dcm_*(dcm_ + jacDcm*temp ) - h_dcm_*getOmega()<<std::endl;
+    
+  }
+
+}
+
+//The explicit instantiation
+template struct mc_impact::dcmWithImpulse<zmpSupportContact, Eigen::Vector3d>;
+template struct mc_impact::dcmWithImpulse<zmpSupportContact, Eigen::Vector2d>;
+
+
+
+} // end of namespace 
